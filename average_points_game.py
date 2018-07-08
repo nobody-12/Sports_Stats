d04478bc6e97fc8f92c6783bdf8a11491f2d3bd5
nobody-12 <40387504+nobody-12@users.noreply.github.com>
2018-07-08 21:23:03 +0530

    Imperfect, Incomplete

---
+++ average_points_game.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+total_counter=-1
+forever=267
+find_avg=0
+score_1 = 0
+change = 2
+with urlopen('https://projects.fivethirtyeight.com/nfl-api/2017/nfl_games_2017.csv') as response:
+    for line in response:
+        line=line.decode('utf-8')
+        if '0' in line:
+            x=line
+            y=x[-8:-3]
+            #print(y)
+            if ',' in y[0]:
+                pass
+            else: 
+                print(y[0:2])
+                print(y[3:5])
