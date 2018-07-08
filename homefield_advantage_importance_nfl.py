5b98474c9272b1215172a800a0665ce387e3fbae
nobody-12 <40387504+nobody-12@users.noreply.github.com>
2018-07-08 16:09:48 +0530

    Finishes Code
    This change enables the code to find the total amount of games played and the number of games won at home. It also prints a final value to the console.

--- homefield_advantage_importance_nfl.py
+++ homefield_advantage_importance_nfl.py
@@ -1,13 +1,20 @@
 from urllib.request import urlopen
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
-forever=1
-
+total_counter=-1
+average=0
+find_avg=0
 with urlopen('https://projects.fivethirtyeight.com/nfl-api/2017/nfl_games_2017.csv') as response:
     for line in response:
         line=line.decode('utf-8')
-        if '0' in line:
+        if '0' or '1' in line:
             x=line
-            print(x[-2:])
-
-
+            y=x[-2:]
+            total_counter=total_counter+1
+            if '0' in y:
+                pass
+            elif '1' in y:
+                find_avg=find_avg+1
+             
+                
+print('The percentage of wins by a home team in the 2017,18 season is',round(find_avg/total_counter,2), end = '%\n')
